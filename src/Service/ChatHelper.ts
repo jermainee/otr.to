ec28fc2e84c7a359c1f7cf4dc8ad33d4ff72304b
Jermaine J. Ernst <jermaine.ernst@icloud.com>
2025-07-23 19:53:54 +0200

    Use crypto api to generate peer id

--- src/Service/ChatHelper.ts
+++ src/Service/ChatHelper.ts
@@ -2,6 +2,13 @@ export default class ChatHelper
 {
     public static generatePeerId(): string
     {
-        return Math.random().toString(36).substring(2, 24);
+        if (!window.crypto || !window.crypto.getRandomValues) {
+            return Math.random().toString(36).substring(2, 24);
+        }
+
+        const array = new Uint8Array(16);
+        window.crypto.getRandomValues(array);
+
+        return Array.from(array, byte => byte.toString(36)).join('').substring(0, 22);
     }
 }
